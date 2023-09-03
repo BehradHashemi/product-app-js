b861514184aef8aeb389e832181415b66350b157
BehradHashemi <behradweb07@gmail.com>
2023-09-03 13:57:07 +0330

    mend
    Update dataset and add add to shop btns

--- js/app.js
+++ js/app.js
@@ -1,16 +1,48 @@
 const searchInput = document.getElementById('search-input');
-const products = document.querySelectorAll('.product-item')
+const products = document.querySelectorAll('.product-item');
+const buttons = document.querySelectorAll('.filter')
+
+const changeClass = (filter) => {
+    buttons.forEach(btn => {
+        if (btn.dataset.filter === filter) {
+            btn.classList.add('selected')
+        } else {
+            btn.classList.remove('selected')
+        }
+    })
+}
 
 const searchHandler = (event) => {
-    const searchValue = event.target.value.toLowerCase();
+    const searchValue = event.target.value.toLowerCase().trim();
     products.forEach((product) => {
         const productName = product.children[1].innerText.toLowerCase();
         if (productName.includes(searchValue)) {
-            product.style.display = 'block'
+            product.style.display = 'block';
         } else {
-            product.style.display = 'none'
+            product.style.display = 'none';
+        }
+    })
+}
+
+const filterHandler = (event) => {
+    const { filter } = event.target.dataset;
+    changeClass(filter);
+
+    products.forEach(product => {
+        const category = product.dataset.category;
+        if (filter === 'all') {
+            product.style.display = 'block';
+        } else {
+            if (filter === category) {
+                product.style.display = 'block';
+            } else {
+                product.style.display = 'none';
+            }
         }
     })
 }
 
 searchInput.addEventListener('keyup', searchHandler)
+buttons.forEach((btn => {
+    btn.addEventListener('click', filterHandler)
+}))
