0d5d651a7fb35f40acd8b42236036cba2b8c1996
BehradHashemi <behradweb07@gmail.com>
2023-09-03 13:34:07 +0330

    add search name products

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const searchInput = document.getElementById('search-input');
+const products = document.querySelectorAll('.product-item')
+
+const searchHandler = (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    products.forEach((product) => {
+        const productName = product.children[1].innerText.toLowerCase();
+        if (productName.includes(searchValue)) {
+            product.style.display = 'block'
+        } else {
+            product.style.display = 'none'
+        }
+    })
+}
+
+searchInput.addEventListener('keyup', searchHandler)
